8f3274a28fc990ae4593a92787094cc0c346fde7
Sevinj-Joy <seva.rasul15@gmail.com>
2022-02-18 20:42:11 +0400

    Baby Joy Project uploading

--- run.py
+++ run.py
@@ -13,8 +13,9 @@ class products(db.Model):
 
 from app.routes import *
 
+
 from admin.routes import *
 
 if __name__ == "__main__":
-    db.create_all()
+    # db.create_all()
     app.run(debug=True)
